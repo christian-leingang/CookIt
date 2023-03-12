2bc3be8c60e53c961fc48e91ab92065dcdafbe39
Christian Leingang <chrilei2000@gmail.com>
2023-03-12 20:50:42 +0100

    continue MVC Pattern

--- src/js/controller.js
+++ src/js/controller.js
@@ -4,14 +4,6 @@ import 'regenerator-runtime/runtime';
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
-
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
@@ -28,10 +20,12 @@ async function controlRecipes() {
     //Rendering recipe
     recipeView.render(model.state.recipe);
   } catch (e) {
-    alert(e);
+    console.log(e);
   }
 }
 
-['hashchange', 'load'].forEach(ev =>
-  window.addEventListener(ev, controlRecipes)
-);
+function init() {
+  recipeView.addHandlerRender(controlRecipes);
+}
+
+init();
